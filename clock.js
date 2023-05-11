ce7c98a96f62535e223f1c8e3b6f79d413b91abf
Petr Bílek <petr.bilek.cz@gmail.com>
2023-05-11 21:11:47 +0200

    Add simple countdown cycle. Add function to start button. Change start button on click to pause button. Return puse button back to start button after countdown ends.

---
+++ clock.js
@@ -0,0 +1,27 @@
+/*
+ ██████╗██╗      ██████╗  ██████╗██╗  ██╗
+██╔════╝██║     ██╔═══██╗██╔════╝██║ ██╔╝
+██║     ██║     ██║   ██║██║     █████╔╝ 
+██║     ██║     ██║   ██║██║     ██╔═██╗ 
+╚██████╗███████╗╚██████╔╝╚██████╗██║  ██╗
+ ╚═════╝╚══════╝ ╚═════╝  ╚═════╝╚═╝  ╚═╝ 
+ */
+
+// Clock function:
+function clock(targetElement) {
+    const timer = setInterval(() => {
+        const date = new Date();
+        const currentTime = date.toLocaleTimeString([], {
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit',
+        });
+        targetElement.innerHTML = `<span class="current-time">${currentTime}</span>`;
+    }, 1000);
+}
+
+// Get clock element:
+const clockDisplay = document.getElementById('clock');
+
+// Run clock inside specified element:
+clock(clockDisplay);
---
+++ timer.js
@@ -0,0 +1,75 @@
+/* 
+████████╗██╗███╗   ███╗███████╗██████╗ 
+╚══██╔══╝██║████╗ ████║██╔════╝██╔══██╗
+   ██║   ██║██╔████╔██║█████╗  ██████╔╝
+   ██║   ██║██║╚██╔╝██║██╔══╝  ██╔══██╗
+   ██║   ██║██║ ╚═╝ ██║███████╗██║  ██║
+   ╚═╝   ╚═╝╚═╝     ╚═╝╚══════╝╚═╝  ╚═╝
+  */
+
+// Countdown function:
+function countdown(countdownInSeconds, ticks) {
+    timer = setInterval(() => {
+        ticks++;
+        console.log(countdownInSeconds - ticks);
+        if (ticks === countdownInSeconds) {
+            stopCountdown();
+            console.log('konzole předtím');
+            controlPannel.replaceChild(buttonStart, buttonPause);
+            console.log('konzole za tím');
+        }
+    }, 1000);
+}
+
+function startCountdown() {
+    hours = inputHours.value;
+    minutes = inputMinutes.value;
+    const countdownInSeconds = hours * 3600 + minutes * 60;
+    ticks = 0;
+    if (countdownInSeconds <= 0) {
+        return;
+    } else {
+        countdown(countdownInSeconds, ticks);
+        controlPannel.replaceChild(buttonPause, buttonStart);
+    }
+}
+
+function stopCountdown() {
+    clearInterval(timer);
+    ticks = 0;
+}
+
+// Get hours input:
+const inputHours = document.getElementById('input-hours');
+// Get minutes input:
+const inputMinutes = document.getElementById('input-minutes');
+// Get control panel element:
+const controlPannel = document.getElementById('control-pannel');
+// Get stop buton:
+const buttonStop = document.getElementById('button-stop');
+// Get reset button:
+const buttonReset = document.getElementById('button-reset');
+// Create start button:
+const buttonStart = document.createElement('button');
+buttonStart.innerText = 'Start';
+// Create pause button:
+const buttonPause = document.createElement('button');
+buttonPause.innerText = 'Pause';
+
+// Insert start button into the DOM:
+controlPannel.insertBefore(buttonStart, buttonStop);
+
+// Variables for input of countdown:
+let hours;
+let minutes;
+
+// Timer variable and ticks variable
+let timer;
+let ticks;
+
+// Run countdown on pressing button start:
+buttonStart.addEventListener('click', () => {
+    hours = inputHours.value;
+    minutes = inputMinutes.value;
+    startCountdown();
+});
