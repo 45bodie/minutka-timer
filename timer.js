2fb03a231a0e5cfd17ac73f68ba1db6771fc7876
Petr Bílek <petr.bilek.cz@gmail.com>
2023-05-21 22:35:17 +0200

    sync

--- timer.js
+++ timer.js
@@ -9,84 +9,84 @@
 
 // Countdown function:
 function countdown(countdownInSeconds, displayElement) {
-  timer = setInterval(() => {
-    ticks++;
-    displayElement.innerText = `${countdownInSeconds - ticks}`;
-    console.log(countdownInSeconds - ticks);
-    if (ticks === countdownInSeconds) {
-      stopCountdown();
-      controlPannel.replaceChild(buttonStart, buttonPause);
-    }
-  }, 1000);
+    timer = setInterval(() => {
+        ticks++;
+        displayElement.innerText = `${countdownInSeconds - ticks}`;
+        console.log(countdownInSeconds - ticks);
+        if (ticks === countdownInSeconds) {
+            stopCountdown();
+            controlPannel.replaceChild(buttonStart, buttonPause);
+        }
+    }, 1000);
 }
 
 // Start countdown function:
 function startCountdown() {
-  hours = inputHours.value;
-  minutes = inputMinutes.value;
-  const countdownInSeconds = hours * 3600 + minutes * 60;
-  ticks = 0;
-  if (countdownInSeconds <= 0) {
-    return;
-  } else {
-    countdown(countdownInSeconds, counterDisplay);
-    controlPannel.replaceChild(buttonPause, buttonStart);
-  }
-  // TODO: Disable inputs while counting!
+    hours = inputHours.value;
+    minutes = inputMinutes.value;
+    const countdownInSeconds = hours * 3600 + minutes * 60;
+    ticks = 0;
+    if (countdownInSeconds <= 0) {
+        return;
+    } else {
+        countdown(countdownInSeconds, counterDisplay);
+        controlPannel.replaceChild(buttonPause, buttonStart);
+    }
+    // TODO: Disable inputs while counting!
 }
 
 // Stop countdown function:
 function stopCountdown() {
-  clearInterval(timer);
-  ticks = 0;
+    clearInterval(timer);
+    ticks = 0;
 }
 
 // Pause countdown function:
 function pauseCountdown() {
-  clearInterval(timer);
-  // TODO: disable inputs while paused
+    clearInterval(timer);
+    // TODO: disable inputs while paused
 }
 
 // Resume countdown function:
 function resumeCountdown() {
-  hours = inputHours.value;
-  minutes = inputMinutes.value;
-  const countdownInSeconds = hours * 3600 + minutes * 60;
-  if (countdownInSeconds <= 0) {
-    return;
-  } else {
-    countdown(countdownInSeconds, counterDisplay);
-    controlPannel.replaceChild(buttonPause, buttonResume);
-  }
+    hours = inputHours.value;
+    minutes = inputMinutes.value;
+    const countdownInSeconds = hours * 3600 + minutes * 60;
+    if (countdownInSeconds <= 0) {
+        return;
+    } else {
+        countdown(countdownInSeconds, counterDisplay);
+        controlPannel.replaceChild(buttonPause, buttonResume);
+    }
 }
 
 // Check actual value of ticks:
 function checkTicks() {
-  return ticks;
+    return ticks;
 }
 
 // Get counter display element:
-const counterDisplay = document.getElementById("counter");
-counterDisplay.innerHTML = "<span>00:00:00</span>";
+const counterDisplay = document.getElementById('counter');
+counterDisplay.innerHTML = '<span>00:00:00</span>';
 // Get hours input:
-const inputHours = document.getElementById("input-hours");
+const inputHours = document.getElementById('input-hours');
 // Get minutes input:
-const inputMinutes = document.getElementById("input-minutes");
+const inputMinutes = document.getElementById('input-minutes');
 // Get control panel element:
-const controlPannel = document.getElementById("control-pannel");
+const controlPannel = document.getElementById('control-pannel');
 // Get stop buton:
-const buttonStop = document.getElementById("button-stop");
+const buttonStop = document.getElementById('button-stop');
 // Get reset button:
-const buttonReset = document.getElementById("button-reset");
+const buttonReset = document.getElementById('button-reset');
 // Create start button:
-const buttonStart = document.createElement("button");
-buttonStart.innerText = "Start";
+const buttonStart = document.createElement('button');
+buttonStart.innerText = 'Start';
 // Create pause button:
-const buttonPause = document.createElement("button");
-buttonPause.innerText = "Pause";
+const buttonPause = document.createElement('button');
+buttonPause.innerText = 'Pause';
 // Create resume button:
-const buttonResume = document.createElement("button");
-buttonResume.innerText = "Resume";
+const buttonResume = document.createElement('button');
+buttonResume.innerText = 'Resume';
 
 // Insert start button into the DOM:
 controlPannel.insertBefore(buttonStart, buttonStop);
@@ -100,35 +100,35 @@ let timer;
 let ticks;
 
 // Run countdown on pressing button start:
-buttonStart.addEventListener("click", () => {
-  hours = inputHours.value;
-  minutes = inputMinutes.value;
-  startCountdown();
+buttonStart.addEventListener('click', () => {
+    hours = inputHours.value;
+    minutes = inputMinutes.value;
+    startCountdown();
 });
 
 // Stop countdown on pressing stop button:
-buttonStop.addEventListener("click", () => {
-  stopCountdown();
-  controlPannel.replaceChild(buttonStart, buttonPause);
-  counterDisplay.innerText = `${inputHours.value}:${inputMinutes.value}:00`;
-  // TODO: click on stop button while countdown is paused
+buttonStop.addEventListener('click', () => {
+    stopCountdown();
+    controlPannel.replaceChild(buttonStart, buttonPause);
+    counterDisplay.innerHTML = `<span>${inputHours.value}:${inputMinutes.value}:00</span>`;
+    // TODO: click on stop button while countdown is paused
 });
 
 // Pause countdown on pause button click:
-buttonPause.addEventListener("click", () => {
-  pauseCountdown();
-  controlPannel.replaceChild(buttonResume, buttonPause);
+buttonPause.addEventListener('click', () => {
+    pauseCountdown();
+    controlPannel.replaceChild(buttonResume, buttonPause);
 });
 
 // Resume countdown on resume button click:
-buttonResume.addEventListener("click", () => {
-  resumeCountdown();
+buttonResume.addEventListener('click', () => {
+    resumeCountdown();
 });
 
 // Reset counter and all around by pressing reset button:
-buttonReset.addEventListener("click", () => {
-  ticks = 0;
-  inputHours.value = "0";
-  inputMinutes.value = "0";
-  counterDisplay.innerText = "00:00:00";
+buttonReset.addEventListener('click', () => {
+    ticks = 0;
+    inputHours.value = '0';
+    inputMinutes.value = '0';
+    counterDisplay.innerHTML = '<span>00:00:00</span>';
 });
